2bf3ca51c39d7115b39848e7e75abd18bba4612a
George Smith <george@georgefelixsmith.co.uk>
2020-10-31 14:23:52 +0000

    Hello world

--- server.rb
+++ server.rb
@@ -3,11 +3,5 @@ require 'bundler' ; Bundler.require
 get '/' do
   content_type :json
 
-  { time: time.now.to_s }.to_json
-end
-
-get '/hello' do
-  content_type :json
-
   { message: 'Hello World' }.to_json
 end
